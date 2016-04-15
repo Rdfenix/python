cd06578950ec811e81961001129c77175de3f5a6
unknown <rudnei.oliveira@PC-TI-27.i-value.int>
2016-04-15 15:24:59 -0300

    Estudos Relacionados ao Python

---
+++ array.py
@@ -0,0 +1,14 @@
+# Definicao de array em python
+my_places = ['school of rock', 'i-value', 'house']
+print(my_places)
+
+# Adicionar mais itens no array
+my_places.append('street')
+my_places.append("friend's house")
+
+print(my_places)
+
+# Deletar um conteudo
+del my_places[3]
+
+print(my_places)
