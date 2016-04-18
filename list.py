96db6706fe59072b4222fb2bc16d2afb23dcd343
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-04-18 14:02:43 -0300

    estudo python

---
+++ list.py
@@ -0,0 +1,15 @@
+breakefast = ['spam n eggs', 'sapm n jam', 'spam n ham']
+lunch = ['slt(spam-lettuce-tomato)', 'pb&s(pb & spam)']
+dinner = ['spalad', 'spamghetti', 'spam noodle soup']
+
+print(breakefast)
+print(lunch)
+print(dinner)
+
+menus = [
+    ['spam n eggs', 'sapm n jam', 'spam n ham'],
+    ['slt(spam-lettuce-tomato)', 'pb&s(pb & spam)'],
+    ['spalad', 'spamghetti', 'spam noodle soup']
+]
+
+print('menu: \t',menus[2])
