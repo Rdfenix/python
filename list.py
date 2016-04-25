5a26a95d4f5a493ad8facd502ec5a157691bb3b3
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-04-25 18:17:19 -0300

    Estudo sobre Python

--- list.py
+++ list.py
@@ -6,10 +6,9 @@ print(breakefast)
 print(lunch)
 print(dinner)
 
-menus = [
-    'breakefast':['spam n eggs', 'sapm n jam', 'spam n ham'],
+menus = ['breakefast':['spam n eggs', 'sapm n jam', 'spam n ham'],
     'lunch': ['slt(spam-lettuce-tomato)', 'pb&s(pb & spam)'],
     'dinner': ['spalad', 'spamghetti', 'spam noodle soup']
 ]
 
-print('menu: \t',menus[2])
+print('menu: \t',menus[3])
