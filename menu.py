b27a227f9cb275ff6c6688e7ac2aa90be5e28dd2
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-04-25 17:34:53 -0300

    Estudo sobre Python

--- menu.py
+++ menu.py
@@ -2,5 +2,7 @@ menu_prices = {
 	'Knackered spam': 0.50, 'Pip pip Spam': 1.50, 'Squidgy Spam': 2.50, 'Smashing Spam': 3.50
 }
 
+# faz um For buscando os valores contidos no dicionario e imprimindo os dois valores desse array.
+#sep='' retira o espa~ço existente entre os valores.
 for name, price in menu_prices.items():
 	print(name, ': $', format(price, '.2f'), sep='')
